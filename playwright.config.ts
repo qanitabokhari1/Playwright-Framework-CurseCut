8dfcabbd791d223c75548cb94fd0cdbf69f39db8
Qanita Syed <33183837+qanitabokhari1@users.noreply.github.com>
2025-11-13 15:28:09 +0500

    fix: linting

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,7 +47,7 @@ export default defineConfig({
     video: 'retain-on-failure',
 
     /* Set a consistent viewport for local and CI (e.g., GitHub Actions) */
-    viewport: { width: 1728, height: 1117 }, // Adjust size 
+    viewport: { width: 1728, height: 1117 }, // Adjust size
   },
 
   /* Configure projects for major browsers */
